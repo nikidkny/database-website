895e2f31e7774f2b778c4dda40dc22ce783b3e7b
Nikolett Dékány <niko703j@stud.kea.dk>
2022-02-16 19:32:40 +0100

    added API, endpoint, articles to html

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-
+// <!--the endpoint https://friends-5d79.restdb.io/rest/database-tv-shows
+// the API key 620d387c34fd621565858693 -->
+const url = "https://friends-5d79.restdb.io/rest/database-tv-shows";
+const options = {
+  headers: {
+    "x-apikey": "620d387c34fd621565858693",
+  },
+};
+fetch(url, options)
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((e) => {
+    console.error("An error occured:", e.message);
+  });
 /*burger menu*/
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
