03c86d014b9f72e6363b1aaa1db78620217b190a
Nancy <nanc0603@stud.kea.dk>
2022-02-16 22:33:24 +0100

    start javascript

---
+++ main.js
@@ -0,0 +1,14 @@
+const url = "https://friends-5d79.restdb.io/rest/database-tv-shows";
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => showSeries(data));
+
+function showSeries(tvshow) {
+  console.log(tvshow);
+  document.querySelector(".series").textContent = tvshow.id;
+  //   document.querySelector(".series").textContent = tvshow.description;
+  //   document.querySelector(
+  //     "img.seriesImage"
+  //   ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  //   document.querySelector("img.productImage").alt = product.productdisplayname;
+}
